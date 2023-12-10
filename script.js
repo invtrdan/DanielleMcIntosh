73d2647f57c7accaaaa60f1fd358e5bfa962cbbd
Danielle McIntosh <danielle.mcintosh@bison.howard.edu>
2023-12-10 11:27:43 -0500

    make it pretty

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    new Typed('#typed', {
-        strings: ["Software Engineer", "Data Scientist", "Coffee Lover", "Jamaican Innovator"],
-        typeSpeed: 50,
-        backSpeed: 25,
-        smartBackspace: true,
-        loop: true
-    });
-});
