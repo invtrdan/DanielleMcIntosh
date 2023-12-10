a9bf9000a092fed9702131db7176bea0861bb8b5
Danielle McIntosh <danielle.mcintosh@bison.howard.edu>
2023-12-10 10:36:00 -0500

    created script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new Typed('#typed', {
+        strings: ["a passionate front-end developer", "an enthusiastic coder", "a creative designer"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        smartBackspace: true,
+        loop: true
+    });
+});
