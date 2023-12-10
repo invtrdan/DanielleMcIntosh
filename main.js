d9c2ea4e6dc5881afc472559f8dcd57c3fc21cf1
Danielle McIntosh <danielle.mcintosh@bison.howard.edu>
2023-12-10 13:58:55 -0500

    added files

---
+++ main.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+const typed = new Typed('.multiple', {
+    strings: ["Software Engineer", "Data Scientist", "Coffee Lover", "Jamaican Innovator", "Lifelong Learner"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+});
